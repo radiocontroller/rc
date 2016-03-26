f94d88ab6cbac192699bb74c1dc4a62d07d7bde1
radiocontroller <302643159@qq.com>
2016-03-26 21:30:57 +0800

    将vendor下css样式移动到lib下

--- app/services/authenticate_user_service.rb
+++ app/services/authenticate_user_service.rb
@@ -8,10 +8,11 @@ class AuthenticateUserService
   end
 
   def call
+    return Status.new(success: false, alert: '该帐号已被限制登录') if @user.deleted
+    return Status.new(success: false, alert: '帐号未激活，请先进入邮箱激活') unless @user.status
     unless @user.present? && @user.valid_password?(@password)
       return Status.new(success: false, alert: '邮箱或密码错误')
     end
-    return Status.new(success: false, alert: '帐号未激活，请先进入邮箱激活') unless @user.status
     Status.new(success: true)
   end
 
