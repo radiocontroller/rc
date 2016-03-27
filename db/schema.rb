1599e6c2aeb0d0c64568fa7f67c89cf12da5f5a6
radiocontroller <302643159@qq.com>
2016-03-27 10:20:30 +0800

    显示flowtwindow

--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 20160324113456) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "username",                               null: false
     t.string   "avatar"
+    t.string   "username",                               null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
