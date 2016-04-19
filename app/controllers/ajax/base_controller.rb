1a67b68add8ea58239755b0f1ef6e281855e41bb
radiocontroller <302643159@qq.com>
2016-04-19 20:59:52 +0800

    禁止非管理员用户直接通过ajax接口进行操作

--- app/controllers/ajax/base_controller.rb
+++ app/controllers/ajax/base_controller.rb
@@ -1,5 +1,13 @@
 module Ajax
   class BaseController < ActionController::Base
+    layout false
     respond_to :js
+    before_action :verify_admin
+
+    private
+
+      def verify_admin
+        render :file => "/public/500.html", layout: false, status: 500 if !current_user.try(:admin?)
+      end
   end
 end
