b5ca43dc7b91ec1dd1b62f2e082cb4f19e76313f
radiocontroller <302643159@qq.com>
2016-04-20 21:44:22 +0800

    调整样式美化界面和优化交互

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < BaseController
   def index
-    @articles = Article.normal
+    q = Article.normal.ransack(params[:q])
+    @articles = q.result.order('id desc')
+    set_search_value
   end
 
   def show
     @article = Article.normal.find(params[:id])
   end
+
+  private
+
+    def set_search_value
+      @title = (params[:q] || {})[:title_cont]
+    end
 end
