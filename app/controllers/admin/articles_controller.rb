40ffa317ab25db7fe03e8902a1fdab7b2a4762b0
radiocontroller <302643159@qq.com>
2016-04-20 19:53:20 +0800

    添加提示信息公用模板

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -4,10 +4,11 @@ module Admin
     before_action :set_article, only: [:edit, :update]
 
     def index
-      @articles = Article.normal
+      @articles = Article.normal.order('id desc')
     end
 
     def new
+      @article = Article.normal.new
     end
 
     def edit
@@ -15,12 +16,16 @@ module Admin
 
     def update
       @article.update(article_params)
-      redirect_to admin_articles_path
+      redirect_to admin_articles_path, notice: '更新成功!'
     end
 
     def create
-      Article.create(title: params[:title], content: params[:content])
-      redirect_to new_admin_article_path
+      article = Article.new(article_params)
+      if article.save
+        redirect_to admin_articles_path, notice: '创建成功!'
+      else
+        redirect_to new_admin_article_path, alert: article.errors.full_messages
+      end
     end
 
     private
