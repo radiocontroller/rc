ed135f801de23bb8131b4ac8b4cdea3b44312a78
radiocontroller <302643159@qq.com>
2016-04-23 14:45:35 +0800

    用户登陆后不允许再进入注册,登陆相关界面

--- app/controllers/devise/base_controller.rb
+++ app/controllers/devise/base_controller.rb
@@ -2,4 +2,13 @@ class Devise::BaseController < ApplicationController
   layout 'accounts'
 
   protect_from_forgery with: :exception
+
+  before_action :verify_user_present, only: [:new]
+
+  private
+
+    def verify_user_present
+      redirect_to root_path if current_user.present?
+    end
+
 end
