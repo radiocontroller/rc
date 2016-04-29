30d5fbe5b31241ead6e2a671dfe688658b447dcf
radiocontroller <302643159@qq.com>
2016-04-29 09:56:53 +0800

    软连接数据库文件

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :format, :pretty
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml}
+# set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -24,7 +24,7 @@ set :rvm_roles, [:app, :web, :db]
 
 namespace :deploy do
   desc 'soft link to database.yml'
-  task :link_database_file, :roles => [:app, :web, :db] do
+  task :link_database_file, :roles => :web do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 
