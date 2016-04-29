0780af4cfd33c3ab05080023ddb071d06754e09a
radiocontroller <302643159@qq.com>
2016-04-29 14:59:22 +0800

    Revert "邮件发送配置"
    This reverts commit cab03d4438ce6664afe739c1e2684e1fdd96b181.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,7 @@ Rails.application.configure do
       :user_name            => ENV['EMAIL'],
       :password             => ENV['PASSWORD'],
       :authentication       => :plain,
-      :enable_starttls_auto => true,
-      :openssl_verify_mode   => 'none'
+      :enable_starttls_auto => true
     }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
