3d918d5cd6e95bc02d014789f9ec4cac37afeb9e
radiocontroller <302643159@qq.com>
2016-05-02 14:49:30 +0800

    user表添加avatar字段

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422055358) do
+ActiveRecord::Schema.define(version: 20160502064553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,12 +76,12 @@ ActiveRecord::Schema.define(version: 20160422055358) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "avatar"
     t.string   "username",                               null: false
     t.boolean  "admin",                  default: false
     t.json     "video_ids",              default: []
     t.json     "article_ids",            default: []
     t.boolean  "deleted",                default: false
+    t.string   "avatar"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
