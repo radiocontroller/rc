fe49ea187e007a1ba7e69dc23e75a8d8bbeefcd4
radiocontroller <302643159@qq.com>
2016-05-02 15:18:02 +0800

    用户收藏中bug修复

--- app/controllers/collections/articles_controller.rb
+++ app/controllers/collections/articles_controller.rb
@@ -2,6 +2,6 @@ class Collections::ArticlesController < Collections::BaseController
   before_action :set_user
 
   def index
-    @articles = @user.article_ids.map { |id| Article.find(id) }
+    @articles = @user.article_ids.map { |id| Article.normal.find(id) }
   end
 end
