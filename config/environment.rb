9267637492803a22cd2a9cb5a71c4a216e7b7988
radiocontroller <302643159@qq.com>
2016-05-03 13:27:36 +0800

    私密数据存储配置

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # append env variables
-app_env_vars = File.join('/var/www/rc/shared/config', 'app_env_vars.rb')
+app_env_vars = File.join('/home/rcer/work/rails_projects/rc/config/initializers', 'app_env_vars.rb')
 load(app_env_vars) if File.exists?(app_env_vars)
 
 # Initialize the Rails application.
