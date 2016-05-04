9070ec1dee71e59bb548d802955fc20932124f2e
radiocontroller <302643159@qq.com>
2016-05-04 13:36:22 +0800

    view层逻辑提取到helper中

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def has_collected?(resource)
+    return '-o' if current_user.blank? || !current_user.has_collected?(resource)
+  end
+
+  def has_commented?(resource)
+    return '-o' if current_user.blank? || !current_user.has_commented?(resource)
+  end
+
+  def login?
+    return 'js-collect cursor-pointer' if current_user.present?
+  end
+
 end
