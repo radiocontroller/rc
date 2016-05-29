bce5b06166e54cd761b753f5f830d5bb583b9f73
radiocontroller <302643159@qq.com>
2016-05-29 10:12:34 +0800

    配置环境变量

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# append env variables
-app_env_vars = File.join('/var/www/rc/shared/config', 'app_env_vars.rb')
-load(app_env_vars) if File.exists?(app_env_vars)
-
 # Initialize the Rails application.
 Rails.application.initialize!
