71d7ffb66a3ccc93eb1494d830faf2445b281870
zhuangshunhe <zhuangshunhe@zhiping.com>
2016-10-22 10:41:42 +0800

    change pic filename

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -11,4 +11,15 @@ class BaseUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+
 end
