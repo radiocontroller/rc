939b2f707dd88c69333a24d61f61a2159d461a76
zhuangshunhe <zhuangshunhe@zhiping.com>
2016-12-15 12:20:20 +0800

    日志记录优化

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,8 +1,8 @@
 class WelcomesController < ApplicationController
   layout 'welcome'
+  after_action :add_access_log, only: [:index]
 
   def index
-    AccessLogJob.perform_later request.ip
     @battle_planes = BattlePlane.normal.sorted
     @video = Video.normal.homepage
     @banners = BannerPicture.normal.sorted
@@ -16,4 +16,11 @@ class WelcomesController < ApplicationController
     @fpvs = galler_pictures.fpv
     @helicopters = galler_pictures.helicopter
   end
+
+  private
+
+  def add_access_log
+      AccessLogJob.perform_later(request.ip) if "127.0.0.1" != request.ip
+  end
+
 end
--- app/jobs/access_log_job.rb
+++ app/jobs/access_log_job.rb
@@ -2,17 +2,15 @@ class AccessLogJob < ActiveJob::Base
   queue_as :default
 
   def perform(ip)
-    if "127.0.0.1" != ip
-        url = "http://ip.taobao.com/service/getIpInfo.php?ip=#{ip}"
-        json = RestClient.get(url)
-        response = JSON.parse(json)
-        data = response["data"]
-        location = data["country"]
-        location = data["area"] if data["area"].present?
-        location = data["region"] if data["region"].present?
-        location = data["city"] if data["city"].present?
-        AccessLog.create(ip: ip, location: location)
-    end
+    url = "http://ip.taobao.com/service/getIpInfo.php?ip=#{ip}"
+    json = RestClient.get(url)
+    response = JSON.parse(json)
+    data = response["data"]
+    location = data["country"]
+    location = data["area"] if data["area"].present?
+    location = data["region"] if data["region"].present?
+    location = data["city"] if data["city"].present?
+    AccessLog.create(ip: ip, location: location)
   end
 
 end
